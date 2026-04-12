b0b28522c711a9f8307ca4876852a0f8cb750c69
TonNom <macbookpro@192.168.1.3>
2026-04-12 21:08:23 +0000

    Fix root route for Render

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,24 @@
 from rest_framework.routers import DefaultRouter
 from .views import LivreViewSet, AuteurViewSet, EmpruntViewSet
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.http import JsonResponse
 
 router = DefaultRouter()
 router.register(r'livres', LivreViewSet, basename='livres')
 router.register(r'auteurs', AuteurViewSet, basename='auteurs')
 router.register(r'emprunts', EmpruntViewSet, basename='emprunts')
 
+def home(request):
+    return JsonResponse({
+        "message": "API Bibliothèque OK 🚀",
+        "routes": [
+            "/livres/",
+            "/auteurs/",
+            "/emprunts/"
+        ]
+    })
+
 urlpatterns = [
+    path('', home),          # 👈 IMPORTANT
     path('', include(router.urls)),
 ]
--- bibliotheque_project/urls.py
+++ bibliotheque_project/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+def home(request):
+    return HttpResponse("Bibliothèque API OK 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/', include('api.urls')),  # ✅ FIX ICI
-
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path('', home),  # 👈 IMPORTANT
 ]
